ff0fefeed26dd381d7f518c1c3a682281b0a14af
Kshitijpawar <kshitijvijay271199@gmail.com>
2023-02-28 17:47:51 +0530

    scrapy stuff

--- controlscrape/controlscrape/spiders/control_scrape.py
+++ controlscrape/controlscrape/spiders/control_scrape.py
@@ -5,3 +5,16 @@ class ControlSpider(scrapy.Spider):
     start_urls = [
         'https://control.fandom.com/wiki/Category:Collectibles_/_Research_%26_Records'
     ]
+
+    def parse(self, response):
+        research = response.xpath('//a[@class="category-page__member-link"]/@href')
+        for link in research:
+            
+            yield response.follow(link.get(), callback= self.parse_research)
+
+    
+    def parse_research(self, response):
+        research_items = response.xpath('//a[@class="category-page__member-link"]/text()')
+        print(f"this is research items: {research_items}")
+        for item in research_items:
+            print(item.get())
