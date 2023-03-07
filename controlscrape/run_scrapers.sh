cb6e11766be652190ddcf81bb3e8664483be9041
Kshitijpawar <kshitijvijay271199@gmail.com>
2023-03-07 11:05:26 +0530

    shell script changes

--- controlscrape/run_scrapers.sh
+++ controlscrape/run_scrapers.sh
@@ -3,5 +3,7 @@ source /home/kshitij/Desktop/control_project/cenv/bin/activate
 scrapy crawl control_research_scraper
 scrapy crawl control_corr_scraper
 scrapy crawl control_hotline_scraper
+scrapy crawl control_casefiles_scraper
+scrapy crawl control_multimedia_scraper
 cd /home/kshitij/Desktop/control_project/Control_collectibles_app/json_files/
-python merge_json.py research_and_records.json hotline.json correspondence.json
+python merge_json.py research_and_records.json hotline.json correspondence.json case_files.json multimedia.json
